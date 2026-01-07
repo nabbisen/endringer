88215d16cb1e8cc1aef1a7d92a23dbb4ad618eab
nabbisen <nabbisen@scqr.net>
2026-01-07 11:43:47 +0900

    first fn

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+use std::path::Path;
+
+use anyhow::Result;
+
+use crate::core::gix::{self, StatusDigest};
+
+mod core;
+
+pub fn status_digest(repo_path: &Path) -> Result<StatusDigest> {
+    gix::status_digest(repo_path)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = status_digest(Path::new("."));
+        println!("{:?}", result);
+        assert!(result.is_ok());
+    }
+}
