dc9ad47145a2b69163df8f1845301dc4cc47f7f1
nabbisen <nabbisen@scqr.net>
2026-01-07 16:37:49 +0900

    fn to get repo - incompatible change on param of status_digest

--- src/core/gix.rs
+++ src/core/gix.rs
@@ -1,13 +1,15 @@
 use anyhow::{Context, Result};
+use gix::Repository;
 use std::path::Path;
 use std::time::{Duration, UNIX_EPOCH};
 
 use crate::types::StatusDigest;
 
-pub fn status_digest(repo_path: &Path) -> Result<StatusDigest> {
-    // open repository
-    let repo = gix::open(repo_path).context("failed to open git repository")?;
+pub fn repo(repo_path: &Path) -> Result<Repository> {
+    gix::open(repo_path).context("failed to open git repository")
+}
 
+pub fn status_digest(repo: &Repository) -> Result<StatusDigest> {
     // repo name
     let repo_name = repo
         .workdir()
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 
+use ::gix::Repository;
 use anyhow::Result;
 
 use crate::{core::gix, types::StatusDigest};
@@ -7,8 +8,12 @@ use crate::{core::gix, types::StatusDigest};
 mod core;
 pub mod types;
 
-pub fn status_digest(repo_path: &Path) -> Result<StatusDigest> {
-    gix::status_digest(repo_path)
+pub fn repo(repo_path: &Path) -> Result<Repository> {
+    gix::repo(repo_path)
+}
+
+pub fn status_digest(repo: &Repository) -> Result<StatusDigest> {
+    gix::status_digest(repo)
 }
 
 #[cfg(test)]
@@ -16,8 +21,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = status_digest(Path::new("."));
+    fn it_works_repo() {
+        let result = repo(Path::new("."));
+        println!("{:?}", result);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn it_works_status_digest() {
+        let repo = repo(Path::new(".")).expect("failed to get repo");
+        let result = status_digest(&repo);
         println!("{:?}", result);
         assert!(result.is_ok());
     }
